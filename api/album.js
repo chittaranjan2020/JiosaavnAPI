d6844e19f56d93a8f6084e4692c2d4d624ff847e
chittaranjan2020 <84212346+chittaranjan2020@users.noreply.github.com>
2021-07-20 15:37:05 +0530

    Update album.js

--- api/album.js
+++ api/album.js
@@ -4,8 +4,8 @@ module.exports = async(req, res) => {
     const reqId = req.query.id
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate")
-    res.setHeader("Open-Source", "https://github.com/Oshekher/JiosaavnAPI")
-    res.setHeader("Edited-By", "Saksham Shekher, https://github.com/Oshekher/")
+    res.setHeader("Open-Source", "https://github.com/chittaranjan2020/JiosaavnAPI")
+    res.setHeader("Edited-By", "Chittaranjan Maharana, https://github.com/chittaranjan2020/Oshekher/")
 
     axios({
         method: 'get',
@@ -61,7 +61,7 @@ module.exports = async(req, res) => {
                 songs: albumRes,
                 perma_url: data.perma_url,
                 release_date: data.release_date,
-                repo_url: "https://github.com/Oshekher/JiosaavnAPI"
+                repo_url: "https://github.com/chittaranjan2020/JiosaavnAPI"
             })
         })
         .catch(function(error) {
